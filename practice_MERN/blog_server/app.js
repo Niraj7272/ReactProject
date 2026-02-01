db8e1c3ed8dd654af303915c8e9124a17cfab043
Niraj7272 <nc72723@gmail.com>
2026-02-01 23:13:44 +0545

    setup

---
+++ practice_MERN/blog_server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+const port = process.env.PORT || 5000
+
+app.get('/',(req,res)=>{
+    res.send("Hello world from server")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is started on :http://localhost:${port}`);
+})
