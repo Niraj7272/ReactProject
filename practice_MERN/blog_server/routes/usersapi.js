61d2a04161f388bde9a4714326443ba375ceeef1
Niraj7272 <nc72723@gmail.com>
2026-02-12 16:13:17 +0545

    learn multer

--- practice_MERN/blog_server/routes/usersapi.js
+++ practice_MERN/blog_server/routes/usersapi.js
@@ -1,8 +1,27 @@
 const bcrypt = require('bcryptjs');
 const express = require("express");
+const path = require("path");
+const multer = require("multer");
 const router = express.Router();
 
 const user = require("../models/user");
+
+const storage = multer.diskStorage({
+  destination:'./uploads/',
+  filename:function(req,file,cb){
+    cb(null,file.fieldname+"_"+Date.now()+".png");
+  }
+})
+
+const upload = multer({
+  storage:storage,
+  limits:{fileSize:1000000}
+})
+
+router.post('/uploadimage',upload.single('profile_pic'),(req,res)=>{
+res.json({"msg":"File upload successfully"})
+})
+
 
 //http://localhost:5000/api/user/adduser
 router.post("/adduser", async (req, res) => {
@@ -26,6 +45,43 @@ router.get("/viewuser", async(req, res)=>{
   try {
     const users = await user.find();
     res.json(users);
+  } catch (error) {
+    res.status(500).json({'Error': error})
+  }
+})
+
+//http://localhost:5000/api/user/singleuser/1111
+router.get('/singleuser/:userid', async(req,res)=>{
+  const uid = req.params.userid
+  try {
+    const users = await user.findById(uid)
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({'Error': error})
+  }
+})
+
+//http://localhost:5000/api/user/updateuser/1111
+router.put('/updateuser/:userid', async(req,res)=>{
+  const uid = req.params.userid
+  try {
+    const users = await user.findByIdAndUpdate(
+      uid,
+      {$set:req.body},
+      {new:true}
+    )
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({'Error': error})
+  }
+})
+
+//http://localhost:5000/api/user/deleteuser/1111
+router.delete('/deleteuser/:userid', async(req,res)=>{
+  const uid = req.params.userid
+  try {
+    const users = await user.findByIdAndDelete(uid)
+    res.status(200).json({'msg':'User deleted successfully'})
   } catch (error) {
     res.status(500).json({'Error': error})
   }
