93d56a8295498f0c16fe1ecf2fd5a1649ad15b43
Niraj7272 <nc72723@gmail.com>
2026-03-18 23:50:20 +0545

    REGISTRATION FORM

--- practice_MERN/blog_react/src/App.jsx
+++ practice_MERN/blog_react/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+// import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Login from './components/Login'
 import TestBlock from './components/TestBlock'
+import Registration from './components/Registration'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login/>
-      <TestBlock/>
+      <BrowserRouter>
+      <Routes>
+       <Route exact path='/' element={<Registration/>}/>
+       <Route exact path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- practice_MERN/blog_react/src/components/Login.jsx
+++ practice_MERN/blog_react/src/components/Login.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [logindt,setLogindt] = useState({
+    user_email:'',
+    password:'',
+  })
+
+  const handleInputChange = (e) => {
+    const {name,value}= e.target
+        setLogindt({
+            ...logindt,
+            [name]:value,
+        })
+  }
+
+  const handleSubmit = async(e)=>{
+     e.preventDefault()
+            try {
+                const res = await axios.post('http://localhost:5000/api/user/userlogin',logindt)
+                console.log(res);
+            } catch (error) {
+                console.error(error);
+            }
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <table>
+            <tr>
+                <td>User Email:</td>
+                <td>
+                    <input type="text" name='user_email' onChange={handleInputChange} />
+                </td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td>
+                    <input type="password" name='password' onChange={handleInputChange} />
+                </td>
+            </tr>
+            <tr>
+                <td colSpan={2}>
+                    <button onClick={handleSubmit}>Login</button>
+                </td>
+            </tr>
+            <tr>
+                <td colSpan={2}>
+                    If already Register <a href="login">Login here</a> 
+                </td>
+            </tr>
+        </table>
+    </div>
   )
 }
 
